b5d12ef6265bf152b5b5bfde9db97cddec763809
Angela Lograsso <alograss@indiana.edu>
2016-12-22 13:35:26 -0500

    branches suck cock

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using LoLSDK;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : Singleton<GameManager> {
+      public static GameManager Instance;
+
+    //Which stage the game is in
+    public bool stage1;
+    public bool stage2;
+
+    public Text db;
+
+
+	// Use this for initialization
+	void Start () {
+	 LOLSDK.Init ("stump.weathergame");
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Call when changing dialogue shown
+    public void UpdateDB(string newtext){
+    
+        db.text = newtext;        
+
+    }
+}
---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using LoLSDK;
+
+public class Object : MonoBehaviour {
+
+    public bool beenClicked;
+    public bool important;
+    public string description;
+    public GameManager gm;
+
+	// Use this for initialization
+	void Start () {
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Clicked(){
+
+        gm.UpdateDB(description);
+        beenClicked = true;
+    
+    }
+}
