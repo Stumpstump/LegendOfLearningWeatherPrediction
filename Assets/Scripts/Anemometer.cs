727bd952c8175c355bab3e6b021eac64c2f34837
Angela Lograsso <alograss@indiana.edu>
2017-01-06 15:36:04 -0500

    no message

--- Assets/Scripts/Anemometer.cs
+++ Assets/Scripts/Anemometer.cs
@@ -11,10 +11,19 @@ public class Anemometer : MonoBehaviour {
     public Text display;
     
     private bool proper;
+     private bool on;
     public GameManager gm;
+    public Button endbutton;
+        private bool mph;
+        
 	// Use this for initialization
 	void Start () {
+        display.gameObject.SetActive(false);
         proper = false;
+        generate = false;
+        on = false;
+        endbutton.interactable = false;
+        mph = true;
 	}
 	
 	// Update is called once per frame
@@ -29,24 +38,55 @@ public class Anemometer : MonoBehaviour {
         }
 	
         if (proper == true){
-            display.text = gm.windSpeedM.ToString();
-            proper = false;
+            if (mph == true){
+
+            display.text = gm.windSpeedM.ToString() + " MPH"; 
+            }
+            else if (mph == false){
+                display.text = gm.windSpeedK.ToString() + " KPH"; 
+            }
+            endbutton.interactable = true;
+            
         }
 	}
 
+        
      private IEnumerator CycleNumbers() {
         
             yield return new WaitForSeconds(0.2f);
-            display.text = Random.Range(1000,9999).ToString();
+            display.text = Random.Range(100,999).ToString();
     }
 
     public void MPHtoKMH(){
          //manually have values here
-        display.text = gm.windSpeedK.ToString();
+        if (proper == true){
+        mph = false;
+        }
     }
     public void KMHtoMPH(){
-        display.text = gm.windSpeedM.ToString();
+        if (proper == true){
+        mph = true;
+        }
+    }
 
+    public void TurnOn(){
+        if (on == false){
+        display.GetComponent<Text>().text = "000";
+        display.gameObject.SetActive(true);
+        generate = true;
+        on = true;
+        }
+
+        else if (on == true){
+        display.gameObject.SetActive(false);
+        generate = false;
+        on = false;
+        proper = false;
+        sec = 0;
+        
+        }
+
+        
     }
     
 }
