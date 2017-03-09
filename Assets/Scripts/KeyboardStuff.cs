6996ea2df666dc39cf951bb3a5e5c7fa66760420
Angela Lograsso <alograss@indiana.edu>
2017-03-09 15:44:56 -0500

    no message

---
+++ Assets/Scripts/KeyboardStuff.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class KeyboardStuff : MonoBehaviour {
+
+ public bool running = true;
+  public string kBoardString = "";
+ 
+ private string tempString = "";
+ private TouchScreenKeyboard keyboard;
+ private bool iskBoardOpen = false;
+ public Text urgh;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (running == true){
+
+        plshalp();
+        
+        }
+
+	}
+
+    public void turnoff (){
+
+    running = false;
+    }
+
+    public void plshalp(){
+          if ( !iskBoardOpen ) 
+         {
+             keyboard = TouchScreenKeyboard.Open( tempString, TouchScreenKeyboardType.Default);
+             iskBoardOpen = true;
+         }
+         
+         if ( keyboard.done ) 
+         {
+             kBoardString = keyboard.text;
+             tempString = "";
+             iskBoardOpen = false;
+         }
+         else
+         {
+             urgh.text = keyboard.text;
+         }
+
+        }
+}
